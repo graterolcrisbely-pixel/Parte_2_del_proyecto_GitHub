e5517c8b55c964b58a6b09f194dd94f2f9bb390a
graterolcrisbely-pixel <graterolcrisbely@gmail.com>
2026-09-02 17:02:24 -0500

    feact: creacion

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const saludoBtn = document.getElementById("saludoBtn");
+
+if (saludoBtn) {
+    saludoBtn.addEventListener("click", function () {
+        alert("¡Gracias por visitar mi portafolio! Espero que disfrutes conociendo mis proyectos.");
+    });
+}
